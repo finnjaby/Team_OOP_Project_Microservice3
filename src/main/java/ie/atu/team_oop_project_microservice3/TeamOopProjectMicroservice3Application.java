b30c418a2eb158ef93bd6bcd3f74cb4c8e001629
FinnjaBayer <g00470359@atu.ie>
2026-05-20 19:14:11 +0100

    created github for third microservice

---
+++ src/main/java/ie/atu/team_oop_project_microservice3/TeamOopProjectMicroservice3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.team_oop_project_microservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOopProjectMicroservice3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamOopProjectMicroservice3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/team_oop_project_microservice3/TeamOopProjectMicroservice3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.team_oop_project_microservice3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamOopProjectMicroservice3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
