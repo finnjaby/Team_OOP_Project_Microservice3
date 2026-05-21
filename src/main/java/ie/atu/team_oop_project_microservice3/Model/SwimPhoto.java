f9a84ced844166f682099cf2aef2efca61411c19
FinnjaBayer <g00470359@atu.ie>
2026-05-21 16:03:31 +0100

    declared entity

--- src/main/java/ie/atu/team_oop_project_microservice3/Model/SwimPhoto.java
+++ src/main/java/ie/atu/team_oop_project_microservice3/Model/SwimPhoto.java
@@ -1,11 +1,13 @@
 package ie.atu.team_oop_project_microservice3.Model;
 
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class SwimPhoto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
