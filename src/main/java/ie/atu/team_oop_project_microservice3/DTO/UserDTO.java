d6a00ae9fc911e38058cc9e11ee00c1f631f8e33
FinnjaBayer <g00470359@atu.ie>
2026-05-21 17:11:16 +0100

    added user dto

---
+++ src/main/java/ie/atu/team_oop_project_microservice3/DTO/UserDTO.java
@@ -0,0 +1,21 @@
+package ie.atu.team_oop_project_microservice3.DTO;
+
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+
+    public UserDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
